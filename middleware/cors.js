f0ecd2be128307ca7f7461e5bf7d2b275a301997
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-29 22:18:03 +0530

    book store api

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,3 +1,9 @@
-/**
- * Created by meeahmed on 9/2/2018.
- */
+const cors = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
+  res.header("Access-Control-Expose-Headers", "x-auth");
+  next();
+};
+
+module.exports = {cors};
--- routes/carts.js
+++ routes/carts.js
@@ -1,16 +1,63 @@
-var express = require('express');
-var router = express.Router();
-var {Book} = require('../models/book');
+const express = require('express');
+const router = express.Router();
+const {Cart} = require('../models/cart');
+const {ObjectID} = require('mongodb');
+const {authenticate} = require('../middleware/authenticate');
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  Book.find({})
-    .then((books) => {
-      res.send(books);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+router.get('/', authenticate, (req, res) => {
+  Cart.find({
+    uid: req.user._id
+  }).then((carts) => {
+    res.send(carts);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
+
+router.post('/', authenticate, function (req, res) {
+  const cart = new Cart({
+    uid: req.user._id,
+    bookId: new ObjectID(req.body.bookId),
+    quantity: req.body.quantity
+  });
+
+  cart.save().then((cart) => {
+    res.send(cart);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
+router.put('/:id', authenticate, (req, res) => {
+  const _id = req.params.id;
+  const quantity = req.body.quantity;
+  Cart.findOneAndUpdate({_id, uid: req.user._id}, {$set: {quantity}}, {new: true})
+    .then((cart) => {
+      if (!cart) {
+        return res.status(404).send();
+      }
+      res.send(cart);
+    }).catch((e) => {
+    res.status(400).send();
+  })
+});
+
+router.delete('/:id', authenticate, (req, res) => {
+  const _id = req.params.id;
+
+  Cart.findOneAndRemove({
+    _id,
+    uid: req.user._id
+  }).then((cart) => {
+    if (!cart) {
+      return res.status(404).send();
+    }
+    res.send(cart);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 module.exports = router;
