0e8bf7795073a8859a06e8a1d0ab36d9b05166c4
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-30 22:18:28 +0530

    book store api

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://localhost:27017/shopping');
 
 module.exports = {mongoose};
