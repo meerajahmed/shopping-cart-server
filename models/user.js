6996644b2cb3441ef94050513efd423b53d7eca4
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-31 22:18:56 +0530

    book store api

--- models/user.js
+++ models/user.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const TOKEN_SECRET_KEY = 'ToKeN-SeCrEt-KeY';
+const bcrypt = require('bcryptjs');
 
-// {
-//   email: 'andrew@example.com',
-//   password: 'adpsofijasdfmpoijwerew',
-//   tokens: [{
-//     access: 'auth',
-//     token: 'poijasdpfoimasdpfjiweproijwer'
-//   }]
-// }
-
-var User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true
+  },
+  lastName: {
+    type: String,
+    require: true
+  },
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
+    minlength: 6,
+    unique: true
   },
   password: {
     type: String,
@@ -39,4 +37,84 @@ var User = mongoose.model('User', {
   }]
 });
 
-module.exports = {User}
+UserSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  return _.pick(userObject, ['_id', 'firstName', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  const user = this;
+  const access = 'auth';
+  const token = jwt.sign({_id: user._id.toHexString(), access}, TOKEN_SECRET_KEY).toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, TOKEN_SECRET_KEY);
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+
+UserSchema.methods.removeToken = function (token) {
+  const user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+const {User} = require('../models/user');
+const {authenticate} = require('../middleware/authenticate');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// POST /users
+router.post('/', (req, res) => {
+  const body = _.pick(req.body, ['firstName', 'lastName', 'email', 'password']);
+  const user = new User(body);
+
+  user.save().then((user) => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
+
+router.post('/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+router.get('/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+router.delete('/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 
 module.exports = router;
